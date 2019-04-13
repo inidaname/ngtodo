b421e3e1dfe94f6c328dde24c989e29a7900cb96
Hassan SaniSani <saniyhassan@gmail.com>
2019-04-13 02:44:54 +0100

    created type for items

--- api/src/items/items.controller.ts
+++ api/src/items/items.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    @Get()
+    findAll(): string {
+        return 'The get request';
+    }
+
+    @Post()
+    creat(): string {
+        return 'Create Item';
+    }
+}

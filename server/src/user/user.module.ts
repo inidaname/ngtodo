41053022906bb29da032abbfee58d664662ffc94
Hassan Sani <saniyhassan@gmail.com>
2019-04-14 23:57:17 +0100

    setting up

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { USER_SERVICE } from './user.constants';
 
 @Module({
   imports: [],
